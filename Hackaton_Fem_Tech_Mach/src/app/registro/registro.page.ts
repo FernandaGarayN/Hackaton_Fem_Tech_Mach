d942bf665336f50c9c04f5779f787ad7528ee663
Catalina Ramirez Olave <133818820+catarock8@users.noreply.github.com>
2024-04-19 18:06:17 -0400

    Cambios en Registro
    agregue nombres,rut,correo,telefono, y varias cosas

--- Hackaton_Fem_Tech_Mach/src/app/registro/registro.page.ts
+++ Hackaton_Fem_Tech_Mach/src/app/registro/registro.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -6,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registro.page.scss'],
 })
 export class RegistroPage implements OnInit {
+  register!: FormGroup;
+  constructor(
+    private formBuilder: FormBuilder,
+  ) {}
 
-  constructor() { }
-
+async registrar() {
+  /*console.log('intentando registar');
+  const user = await this.authService.register(this.credentials.value);
+  if (user) {
+    const userprofile = {
+      email: this.credentials.get('email')?.value,
+    };
+    const collectionRef = collection(this.firestore, 'users');
+    await addDoc(collectionRef, userprofile);
+    this.router.navigateByUrl('/home', { replaceUrl: true });
+  } else {
+    console.log('error al registrar');
+    await this.toastErrorMessage('El correo que intenta registrar ya existe');
+  }*/
+}
   ngOnInit() {
   }
 
