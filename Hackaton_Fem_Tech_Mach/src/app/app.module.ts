ba791aa403a829493cc73a34586b6cb3de8dac68
Fernanda Garay <m.garay@duocuc.cl>
2024-04-20 16:20:18 -0400

    Arreglo error
    se arregla error que impedía abrir formulario de registro

--- Hackaton_Fem_Tech_Mach/src/app/app.module.ts
+++ Hackaton_Fem_Tech_Mach/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
@@ -17,6 +18,7 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
     BrowserModule, 
     IonicModule.forRoot(), 
     AppRoutingModule,
+    HttpClientModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
